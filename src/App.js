c9d1265e8930e1bd0b4c5f11f4c4a7284d906d81
Mohammed Saqib <31315815+mdsaqib108@users.noreply.github.com>
2021-01-02 17:03:01 +0530

    initialise react and create simple form structure

---
+++ src/App.js
@@ -0,0 +1,191 @@
+import ChatBot from 'react-simple-chatbot';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BotAvt from './assets/vtulogo.png';
+import { ThemeProvider } from 'styled-components';
+
+// all available props
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
+  headerBgColor: '#333',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#333',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
+
+class Review extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      gender: '',
+      age: '',
+    };
+  }
+
+  componentWillMount() {
+    const { steps } = this.props;
+    const { name, gender, age } = steps;
+
+    this.setState({ name, gender, age });
+  }
+
+  render() {
+    const { name, gender, age } = this.state;
+    return (
+      <div style={{ width: '100%' }}>
+        <h3>Summary</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{name.value}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{gender.value}</td>
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{age.value}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+Review.propTypes = {
+  steps: PropTypes.object,
+};
+
+Review.defaultProps = {
+  steps: undefined,
+};
+
+class SimpleForm extends Component {
+  render() {
+    return (
+      <ChatBot
+      headerTitle="Welcome To VTU"
+      botAvatar= {BotAvt}
+        steps={[
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: 'name',
+          },
+          {
+            id: 'name',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}! What is your gender?',
+            trigger: 'gender',
+          },
+          {
+            id: 'gender',
+            options: [
+              { value: 'male', label: 'Male', trigger: '5' },
+              { value: 'female', label: 'Female', trigger: '5' },
+            ],
+          },
+          {
+            id: '5',
+            message: 'How old are you?',
+            trigger: 'age',
+          },
+          {
+            id: 'age',
+            user: true,
+            trigger: '7',
+            validator: (value) => {
+              if (isNaN(value)) {
+                return 'value must be a number';
+              } else if (value < 0) {
+                return 'value must be positive';
+              } else if (value > 120) {
+                return `${value}? Come on!`;
+              }
+
+              return true;
+            },
+          },
+          {
+            id: '7',
+            message: 'Great! Check out your summary',
+            trigger: 'review',
+          },
+          {
+            id: 'review',
+            component: <Review />,
+            asMessage: true,
+            trigger: 'update',
+          },
+          {
+            id: 'update',
+            message: 'Would you like to update some field?',
+            trigger: 'update-question',
+          },
+          {
+            id: 'update-question',
+            options: [
+              { value: 'yes', label: 'Yes', trigger: 'update-yes' },
+              { value: 'no', label: 'No', trigger: 'end-message' },
+            ],
+          },
+          {
+            id: 'update-yes',
+            message: 'What field would you like to update?',
+            trigger: 'update-fields',
+          },
+          {
+            id: 'update-fields',
+            options: [
+              { value: 'name', label: 'Name', trigger: 'update-name' },
+              { value: 'gender', label: 'Gender', trigger: 'update-gender' },
+              { value: 'age', label: 'Age', trigger: 'update-age' },
+            ],
+          },
+          {
+            id: 'update-name',
+            update: 'name',
+            trigger: '7',
+          },
+          {
+            id: 'update-gender',
+            update: 'gender',
+            trigger: '7',
+          },
+          {
+            id: 'update-age',
+            update: 'age',
+            trigger: '7',
+          },
+          {
+            id: 'end-message',
+            message: 'Thanks! Your data was submitted successfully!',
+            end: true,
+          },
+        ]}
+      />
+    );
+  }
+}
+
+
+const ThemedExample = () => (
+  <ThemeProvider theme={theme}>
+    <SimpleForm/>
+  </ThemeProvider>
+);
+
+export default ThemedExample;
