52a022e79599d3203603c0582ce8773d422d94e9
Mohammed Saqib <31315815+mdsaqib108@users.noreply.github.com>
2021-03-28 15:25:07 +0530

    theme styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ChatBot, { Loading } from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import BotAvt from './assets/vtulogo.png';
 
-
+// all available props
+const theme = {
+    background: '#f5f8fb',
+    fontFamily: 'Helvetica Neue,Helvetica,Arial,sans-serif',
+    headerBgColor: '#202121',
+    headerFontColor: '#fff',
+    headerFontSize: '14px',
+    botBubbleColor: '#202121',
+    botFontColor: '#fff',
+    userBubbleColor: '#fff',
+    userFontColor: '#4a4a4a',
+};
 
 class DBPedia extends Component {
     constructor(props) {
@@ -16,6 +29,7 @@ class DBPedia extends Component {
 
         this.triggetNext = this.triggetNext.bind(this);
     }
+
 
     componentWillMount() {
         const self = this;
@@ -98,11 +112,15 @@ DBPedia.defaultProps = {
 };
 
 const ExampleDBPedia = () => (
+    <ThemeProvider theme={theme}>
     <ChatBot
+        headerTitle="Welcome To VTU ChatBot"
+        botAvatar= {BotAvt}
+        recognitionEnable={true}
         steps={[
             {
                 id: '1',
-                message: 'Type something to search on Wikipédia. (Ex.: Brazil)',
+                message: 'Type something to search on VTU',
                 trigger: 'search',
             },
             {
@@ -118,6 +136,8 @@ const ExampleDBPedia = () => (
             },
         ]}
     />
+    </ThemeProvider>
 );
+
 
 export default ExampleDBPedia;
