d5b412092218aca2f818cab2d0466df03748e1aa
Mohammed Saqib <31315815+mdsaqib108@users.noreply.github.com>
2021-06-19 01:20:41 +0530

    chatbot flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ChatBot, { Loading } from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 import BotAvt from './assets/vtulogo.png';
+import vcImage from './assets/vcs-message3.jpg';
 import SearchCollegeByName from './components/searchCollegeByName';
 
 // all available props
@@ -15,8 +16,8 @@ const theme = {
     userFontColor: '#4a4a4a',
 };
 
-function GoToPage(){
-    window.location.href = 'http://www.google.com';
+function research(){
+    window.location.href = 'http://research.vtu.ac.in/';
 }
 
 const ExampleDBPedia = () => (
@@ -29,19 +30,22 @@ const ExampleDBPedia = () => (
             {
                 id: 'welcomeMessage',
                 message: 'Hello! Welcome to Visvesvaraya Technological University. How may I help you?',
-                trigger: 'indexMessage',
+                trigger: 'getStarted',
             },
             {
-                id: 'indexMessage',
+                id: 'getStarted',
                 message: 'Kindly choose from one of the following categories to get started.',
-                trigger: 'indexOptions',
+                trigger: 'mainMenu',
             },
             {
-                id: 'indexOptions',
+                id: 'mainMenu',
                 options: [
                     { value: 1, label: 'About VTU', trigger: 'aboutVTU' },
-                    { value: 2, label: 'Vision, Mission and Mandate', trigger: 'visionMissionMandate' },
-                    { value: 3, label: 'Number 3', trigger: '3' },
+                    // { value: 2, label: 'Vision, Mission and Mandate', trigger: 'visionMissionMandate' },
+                    // { value: 3, label: 'Admission', trigger: 'admission' },
+                    // { value: 4, label: 'Campuses', trigger: 'campuses' },
+                    // { value: 5, label: 'Contact', trigger: 'contact' },
+                    // { value: 6, label: 'Request a Call Back', trigger: 'callBack' },
                 ],
             },
             {
@@ -49,20 +53,63 @@ const ExampleDBPedia = () => (
                 message: 'Visvesvaraya Technological University is a collegiate public state university in Belagavi, ' +
                     'Karnataka established by the Government of Karnataka. Apart from a few notable exceptions, ' +
                     'VTU has authority over engineering education throughout the state of Karnataka.',
-                trigger: 'aboutLoadMore',
+                trigger: 'aboutMenu',
             },
             {
-                id: 'aboutLoadMore',
-                component: <GoToPage />,
+                id: 'aboutMenu',
+                options: [
+                    { value: 1, label: 'VC\'s Message', trigger: 'vcsMessage' },
+                    { value: 2, label: 'Research', trigger: 'research' },
+                    { value: 3, label: 'Vision, Mission and Mandate', trigger: 'vision' },
+                    // { value: 4, label: 'Campuses', trigger: 'campuses' },
+                    // { value: 5, label: 'Contact', trigger: 'contact' },
+                    // { value: 6, label: 'Request a Call Back', trigger: 'callBack' },
+                ],
             },
             {
-                id: 'visionMissionMandate',
-                message: 'haha',
-            },
-            {
-                id: '3',
-                message: 'oops',
-            },
+                id: 'vcsMessage',
+                component: (
+                    <div>
+                      <img class="vcImage" src={vcImage}></img>
+                      <h4 class="vcTitle">Hon’ble Vice Chancellor <br></br><strong>Dr.Karisiddappa</strong></h4>
+                      <p>Our hard-won reputation, as a Premier Technological University in the country, carries with it responsibility as well as opportunity. We must continue to encourage student-faculty interactions. We must also be responsible for our actions and behave accordingly, People look upto us to lead from the front and we must not disappoint them. We shall contribute to the society through the pursuit of education, learning, research and innovations at the highest levels of excellence.</p>
+                      <div class="loadMoreButton"><a href="https://vtu.ac.in/en/vcs-message/">Read More</a></div>                      
+                    </div>
+                ),
+              },
+              {
+                id: 'vision',
+                component: (
+                    <div>
+                      <h4>Vision</h4>
+                      <p>To become an outstanding Technological University at the cutting edge of Science and Technology that produces world class Knowledge-delivery, Research, Extension and Leadership in Technology innovation for Industry and Society.</p>                   
+                    </div>
+                ),
+                trigger: 'mission',
+              },
+              {
+                id: 'mission',
+                component: (
+                    <div>
+                      <h4>Mission</h4>
+                      <p>To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>                   
+                    </div>
+                ),
+                trigger: 'mandate',
+              },
+              {
+                id: 'mandate',
+                component: (
+                    <div>
+                      <h4>Mission</h4>
+                      <p>The Visvesvaraya Technological University has been established by the Government of the Karnataka in order to Promote planned and sustainable development of technical education consistent with state and national policies.</p>                   
+                    </div>
+                ),
+              },
+              {
+                id: 'research',
+                component: <research />,
+              },
         ]}
     />
     </ThemeProvider>
